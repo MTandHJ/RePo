e2e5c608fafe909fbbbf153eb4969b236b05a523
MTandHJ <congxueric@gmail.com>
2024-02-29 16:13:51 +0800

    add GPR-GNN

---
+++ General/GPR-GNN/README.md
@@ -0,0 +1,16 @@
+
+
+# GPR-GNN
+
+[[official-code](https://github.com/jianhao2016/GPRGNN)]
+
+
+## Usage
+
+Run with full-ranking
+
+    python main.py --config=configs/xxx.yaml --ranking=full
+
+or with sampled-based ranking
+
+    python main.py --config=configs/xxx.yaml --ranking=pool
---
+++ General/GPR-GNN/main.py
@@ -0,0 +1,207 @@
+
+
+from typing import Dict, Optional, Union
+
+import torch
+import torch.nn as nn
+import torch_geometric.transforms as T
+from torch_geometric.data.data import Data
+from torch_geometric.nn import LGConv
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+
+import freerec
+from freerec.data.fields import FieldModuleList
+from freerec.data.tags import USER, SESSION, ITEM, TIMESTAMP, ID
+
+freerec.declare(version='0.6.3')
+
+cfg = freerec.parser.Parser()
+cfg.add_argument("-eb", "--embedding-dim", type=int, default=64)
+cfg.add_argument("--layers", type=int, default=3)
+
+cfg.add_argument("--alpha", type=float, default=0.1, help="Parameter for APPNP-like initalization")
+
+cfg.set_defaults(
+    description="GPR-GNN",
+    root="../../data",
+    dataset='Gowalla_10100811_Chron',
+    epochs=1000,
+    batch_size=2048,
+    optimizer='adam',
+    lr=1e-3,
+    weight_decay=1e-4,
+    seed=1
+)
+cfg.compile()
+
+
+class GPRGNN(freerec.models.RecSysArch):
+
+    def __init__(
+        self, fields: FieldModuleList, 
+        graph: Data,
+        num_layers: int = 3
+    ) -> None:
+        super().__init__()
+
+        self.fields = fields
+        self.conv = LGConv(False)
+        self.num_layers = num_layers
+        self.User, self.Item = self.fields[USER, ID], self.fields[ITEM, ID]
+        self.graph = graph
+        self.temp = nn.parameter.Parameter(
+            torch.rand(num_layers + 1)
+        )
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.)
+            elif isinstance(m, nn.Embedding):
+                nn.init.normal_(m.weight, std=1.e-4)
+            elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+                nn.init.constant_(m.weight, 1.)
+                nn.init.constant_(m.bias, 0.)
+
+        for l in range(self.num_layers):
+            self.temp.data[l] = cfg.alpha * (1 - cfg.alpha) ** l
+        self.temp.data[-1] = (1 - cfg.alpha) ** l
+
+    @property
+    def graph(self):
+        return self.__graph
+
+    @graph.setter
+    def graph(self, graph: Data):
+        self.__graph = graph
+        T.ToSparseTensor()(self.__graph)
+        self.__graph.adj_t = gcn_norm(
+            self.__graph.adj_t, num_nodes=self.User.count + self.Item.count,
+            add_self_loops=False
+        )
+
+    def to(
+        self, device: Optional[Union[int, torch.device]] = None, 
+        dtype: Optional[Union[torch.dtype, str]] = None, 
+        non_blocking: bool = False
+    ):
+        if device:
+            self.graph.to(device)
+        return super().to(device, dtype, non_blocking)
+
+    def forward(self):
+        userEmbs = self.User.embeddings.weight
+        itemEmbs = self.Item.embeddings.weight
+        features = torch.cat((userEmbs, itemEmbs), dim=0).flatten(1) # N x D
+        allFeats = [features]
+        for _ in range(self.num_layers):
+            features = self.conv(features, self.graph.adj_t)
+            allFeats.append(features)
+        allFeats = torch.cat(allFeats, dim=-1) # (B, D, L + 1)
+        avgFeats = torch.einsum('BDL,L->BD', [allFeats, self.temp])
+        userFeats, itemFeats = torch.split(avgFeats, (self.User.count, self.Item.count))
+        return userFeats, itemFeats
+
+    def predict(self, users: torch.Tensor, items: torch.Tensor):
+        userFeats, itemFeats = self.forward()
+        userFeats = userFeats[users] # B x 1 x D
+        itemFeats = itemFeats[items] # B x n x D
+        userEmbs = self.User.look_up(users) # B x 1 x D
+        itemEmbs = self.Item.look_up(items) # B x n x D
+        return torch.mul(userFeats, itemFeats).sum(-1), userEmbs, itemEmbs
+
+    def recommend_from_full(self):
+        return self.forward()
+
+
+class CoachForGPRGNN(freerec.launcher.GenCoach):
+
+    def reg_loss(self, userEmbds, itemEmbds):
+        userEmbds, itemEmbds = userEmbds.flatten(1), itemEmbds.flatten(1)
+        loss = userEmbds.pow(2).sum() + itemEmbds.pow(2).sum()
+        loss = loss / userEmbds.size(0)
+        return loss / 2
+
+    def train_per_epoch(self, epoch: int):
+        for data in self.dataloader:
+            users, positives, negatives = [col.to(self.device) for col in data]
+            items = torch.cat(
+                [positives, negatives], dim=1
+            )
+            scores, users, items = self.model.predict(users, items)
+            pos, neg = scores[:, 0], scores[:, 1]
+            reg_loss = self.reg_loss(users.flatten(1), items.flatten(1)) * self.cfg.weight_decay
+            loss = self.criterion(pos, neg) + reg_loss
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            
+            self.monitor(loss.item(), n=scores.size(0), mode="mean", prefix='train', pool=['LOSS'])
+
+
+def main():
+
+    dataset = getattr(freerec.data.datasets.general, cfg.dataset)(cfg.root)
+    User, Item = dataset.fields[USER, ID], dataset.fields[ITEM, ID]
+
+    # trainpipe
+    trainpipe = freerec.data.postprocessing.source.RandomIDs(
+        field=User, datasize=dataset.train().datasize
+    ).sharding_filter().gen_train_uniform_sampling_(
+        dataset, num_negatives=1
+    ).batch(cfg.batch_size).column_().tensor_()
+
+    validpipe = freerec.data.dataloader.load_gen_validpipe(
+        dataset, batch_size=512, ranking=cfg.ranking
+    )
+    testpipe = freerec.data.dataloader.load_gen_testpipe(
+        dataset, batch_size=512, ranking=cfg.ranking
+    )
+
+    tokenizer = FieldModuleList(dataset.fields)
+    tokenizer.embed(
+        cfg.embedding_dim, ID
+    )
+    model = GPRGNN(
+        tokenizer, dataset.train().to_graph((USER, ID), (ITEM, ID)), num_layers=cfg.layers
+    )
+
+    if cfg.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=cfg.lr, 
+            momentum=cfg.momentum,
+            nesterov=cfg.nesterov,
+        )
+    elif cfg.optimizer == 'adam':
+        optimizer = torch.optim.Adam(
+            model.parameters(), lr=cfg.lr,
+            betas=(cfg.beta1, cfg.beta2),
+        )
+    criterion = freerec.criterions.BPRLoss()
+
+    coach = CoachForGPRGNN(
+        dataset=dataset,
+        trainpipe=trainpipe,
+        validpipe=validpipe,
+        testpipe=testpipe,
+        model=model,
+        criterion=criterion,
+        optimizer=optimizer,
+        lr_scheduler=None,
+        device=cfg.device
+    )
+    coach.compile(
+        cfg, 
+        monitors=['loss', 'recall@10', 'recall@20', 'ndcg@10', 'ndcg@20'],
+        which4best='ndcg@20'
+    )
+    coach.fit()
+
+
+if __name__ == "__main__":
+    main()
